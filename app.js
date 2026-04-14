ff882139897bf1a66b234af515af5b662be1e0f5
deepak mistry <mistrydeepak1028@gmail.com>
2026-04-14 19:22:00 +0530

    change url after search

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const reviewRouter = require("./routes/review");
 const userRouter = require("./routes/user");
 const ExpressError = require("./utils/ExpressError");
 
-// const MONGO_URL = process.env.MONGO_URL;
-const MONGO_URL ="mongodb://127.0.0.1:27017/wanderLust1";
+const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL ="mongodb://127.0.0.1:27017/wanderLust1";
 
 mongoose.connect(MONGO_URL)
   .then(() => console.log("Connected to DB"))
